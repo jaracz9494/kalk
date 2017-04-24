594842571ecd8486720f9cd90bfc7301d0aa6066
jaracz9494 <jaracz9494@wp.pl>
2017-04-24 17:22:10 +0200

    Add files via upload

---
+++ Kalkulattor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulattor;
+
+/**
+ *
+ * @author Student
+ */
+public class Kalkulattor {
+    
+    public int suma (int a, int b) {
+        return a+b;
+    }
+    
+    public int roznica (int a, int b) {
+        return a-b;
+    }
+    
+    public int iloczyn (int a, int b) {
+        return a*b;
+    }
+    
+    public int iloraz (int a, int b) {
+        return a/b;
+    }
+    
+    public int potega (int a, int b) {
+        return a^b;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
